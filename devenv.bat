ce4dad49e8be155df94d9d0518fce9afa564e691
archangel <archangel@asrai.io>
2021-09-28 00:27:32 +0200

    updated

--- devenv.bat
+++ devenv.bat
@@ -1,1 +1,13 @@
-"D:\Apps\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" %*
+echo off
+setlocal EnableDelayedExpansion
+
+set c_devenv="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
+set d_devenv="D:\Apps\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
+
+if exist %c_devenv% (
+    %c_devenv% %*
+) else if exist %d_devenv% (
+    %d_devenv% %*
+) else (
+    echo "can't find devenv.exe"
+)
